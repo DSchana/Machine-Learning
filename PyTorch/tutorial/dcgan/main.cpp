133e73dc7fc6fb3e783e868a85acacb08e811b70
Dilpreet Chana <dschana6@gmail.com>
2020-09-30 13:49:09 -0400

    gcgan tutorial

--- PyTorch/tutorial/dcgan/main.cpp
+++ PyTorch/tutorial/dcgan/main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstdio>
 #include <torch/torch.h>
 #include <iostream>
 
@@ -8,7 +9,7 @@ const int64_t kNumberOfEpochs = 30;               // The number of epochs to tra
 const int64_t kCheckpointEvery = 200;             // After how many batches to create a new checkpoint periodically
 const int64_t kNumberOfSamplesPerCheckpoint =10;  // How many images to sample at every checkpoint
 const int64_t kLogInterval = 10;                  // After how many batches to log a new update with the loss value.
-const bool kRestoreFromCheckpoint = false;        // Set to 'true' to restore models and optimizers from previously saved checkpoints
+const bool kRestoreFromCheckpoint = true;         // Set to 'true' to restore models and optimizers from previously saved checkpoints
 const char* kDataFolder = "./data";               // Where to find the MNIST dataset
 
 struct DCGANGeneratorImpl : torch::nn::Module {
@@ -45,8 +46,120 @@ struct DCGANGeneratorImpl : torch::nn::Module {
 };
 TORCH_MODULE(DCGANGenerator);
 
-DCGANGenerator generator();
-
 int main() {
+    torch::manual_seed(1);
+
+    // Create the device we pass around based on whether CUDA is available
+    torch::Device device(torch::kCPU);
+    if (torch::cuda::is_available()) {
+        std::cout << "CUDA is available! Training on GPU." << std::endl;
+        device = torch::Device(torch::kCUDA);
+    }
+    else {
+        std::cout << "CUDA not available. Training on CPU." << std::endl;
+    }
+
+    DCGANGenerator generator(kNoiseSize);
+    generator->to(device);
+
+    torch::nn::Sequential discriminator(
+            // Layer 1
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 64, 4).stride(2).padding(1).bias(false)),
+            torch::nn::LeakyReLU(torch::nn::LeakyReLUOptions().negative_slope(0.2)),
+
+            // Layer 2
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, 4).stride(2).padding(1).bias(false)),
+            torch::nn::BatchNorm2d(128),
+            torch::nn::LeakyReLU(torch::nn::LeakyReLUOptions().negative_slope(0.2)),
+
+            // Layer 3
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 256, 4).stride(2).padding(1).bias(false)),
+            torch::nn::BatchNorm2d(256),
+            torch::nn::LeakyReLU(torch::nn::LeakyReLUOptions().negative_slope(0.2)),
+
+            // Layer 4
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(256, 1, 3).stride(1).padding(0).bias(false)),
+            torch::nn::Sigmoid()
+    );
+    discriminator->to(device);
+
+    // Assume the MNIST dataset is available under 'kDataFolder'
+    auto dataset = torch::data::datasets::MNIST(kDataFolder)
+            .map(torch::data::transforms::Normalize<>(0.5, 0.5))
+            .map(torch::data::transforms::Stack<>());
+
+    const int64_t batches_per_epoch = std::ceil(dataset.size().value() / static_cast<double>(kBatchSize));
+
+    auto data_loader = torch::data::make_data_loader(std::move(dataset), torch::data::DataLoaderOptions().batch_size(kBatchSize).workers(2));
+
+    torch::optim::Adam generator_optimizer(generator->parameters(), torch::optim::AdamOptions(2e-4).betas(std::make_tuple(0.5, 0.5)));
+    torch::optim::Adam discriminator_optimizer(discriminator->parameters(), torch::optim::AdamOptions(2e-4).betas(std::make_tuple(0.5, 0.5)));
+
+    if (kRestoreFromCheckpoint) {
+        torch::load(generator, "generator-checkpoint.pt");
+        torch::load(generator_optimizer, "generator-optimizer-checkpoint.pt");
+        torch::load(discriminator, "discriminator-checkpoint.pt");
+        torch::load(discriminator_optimizer, "discriminator-optimizer-checkpoint.pt");
+    }
+
+    int64_t checkpoint_counter = 1;
+    for (int64_t epoch = 1; epoch <= kNumberOfEpochs; epoch++) {
+        int64_t batch_index = 0;
+        for (torch::data::Example<>& batch : *data_loader) {
+            // Train discriminator with real images
+            discriminator->zero_grad();
+            torch::Tensor real_images = batch.data.to(device);
+            torch::Tensor real_labels = torch::empty(batch.data.size(0), device).uniform_(0.8, 1.0);
+            torch::Tensor real_output = discriminator->forward(real_images);
+            torch::Tensor d_loss_real = torch::binary_cross_entropy(real_output, real_labels);
+            d_loss_real.backward();
+
+            // Train discriminator with fake images
+            torch::Tensor noise = torch::randn({ batch.data.size(0), kNoiseSize, 1, 1 }, device);
+            torch::Tensor fake_images = generator->forward(noise);
+            torch::Tensor fake_labels = torch::zeros(batch.data.size(0), device);
+            torch::Tensor fake_output = discriminator->forward(fake_images.detach());
+            torch::Tensor d_loss_fake = torch::binary_cross_entropy(fake_output, fake_labels);
+            d_loss_fake.backward();
+
+            torch::Tensor d_loss = d_loss_real + d_loss_fake;
+            discriminator_optimizer.step();
+
+            // Train generator
+            generator->zero_grad();
+            fake_labels.fill_(1);
+            fake_output = discriminator->forward(fake_images);
+            torch::Tensor g_loss = torch::binary_cross_entropy(fake_output, fake_labels);
+            g_loss.backward();
+            generator_optimizer.step();
+            batch_index++;
+
+            if (batch_index % kLogInterval == 0) {
+                std::printf("\r[%2ld/%2ld][%3ld/%3ld] D_loss: %.4f | G_loss: %.4f\n",
+                            epoch,
+                            kNumberOfEpochs,
+                            batch_index,
+                            batches_per_epoch,
+                            d_loss.item<float>(),
+                            g_loss.item<float>());
+            }
+
+            if (batch_index % kCheckpointEvery == 0) {
+                // Checkpoint the model and optimizer state
+                torch::save(generator, "generator-checkpoint.pt");
+                torch::save(generator_optimizer, "generator-optimizer-checkpoint.pt");
+                torch::save(discriminator, "discriminator-checkpoint.pt");
+                torch::save(discriminator_optimizer, "discriminator-optimizer-checkpoint.pt");
+
+                // Sample the generator and save the images
+                torch::Tensor samples = generator->forward(torch::randn({ kNumberOfSamplesPerCheckpoint, kNoiseSize, 1, 1 }, device));
+                torch::save((samples + 1.0) / 2.0, torch::str("dcgan-sample-", checkpoint_counter, ".pt"));
+                std::cout << "\n-> checkpoint " << ++checkpoint_counter << std::endl;
+            }
+        }
+    }
+
+    std::cout << "Training complete!" << std::endl;
+
     return 0;
 }
